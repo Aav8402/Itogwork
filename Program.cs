511e17068a6868e1d728c0fce04d21da586df295
Alexey <aav8402@mail.ru>
2022-11-28 14:55:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Нарисать программу, которая из имеющегося массива строк, длина которых меньше
+// либо равна 3 символа. Первоначальный массивможно ввеести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] array1 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+void Array(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Array(array1, array2);
+PrintArray(array2);
+
